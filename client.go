601cef5ab343c04a55ad99fd56701e9c828c0551
Mozgovoy Andrey <mozgovoy.andrey@gmail.com>
2020-05-21 12:02:34 +0300

    add method domain/get_prices

---
+++ client.go
@@ -0,0 +1,61 @@
+package go_regruclient
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"net/http"
+)
+
+type Client struct {
+	apiUri   string
+	login    string
+	password string
+	client   *http.Client
+}
+
+func NewClient(login, password string) *Client {
+	return &Client{
+		apiUri:   "https://api.reg.ru/api/regru2/",
+		login:    login,
+		password: password,
+		client:   &http.Client{},
+	}
+}
+
+func (c *Client) request(method string, params map[string]string) ([]byte, error) {
+	req, err := http.NewRequest("GET", c.apiUri+method, nil)
+	if err != nil {
+		return []byte(""), errors.Wrap(err, "create new request")
+	}
+
+	q := req.URL.Query()
+	q.Add("username", c.login)
+	q.Add("password", c.password)
+	q.Add("output_format", "json")
+	for paramName, paramValue := range params {
+		q.Add(paramName, paramValue)
+	}
+	req.URL.RawQuery = q.Encode()
+
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		fmt.Println("clientDo")
+		fmt.Println(err)
+		return []byte(""), err
+	}
+
+	defer resp.Body.Close()
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("ReadAll body")
+		fmt.Println(err)
+		return []byte(""), err
+	}
+
+	fmt.Println(resp.Status)
+	fmt.Println(string(respBody))
+	return respBody, nil
+}
---
+++ domain.go
@@ -0,0 +1,46 @@
+package go_regruclient
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+)
+
+func (c *Client) DomainGetPrice() (*DomainGetPriceResponse, error) {
+	body, err := c.request("domain/get_prices", map[string]string{
+		"show_renew_data": "1",
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "send request")
+	}
+
+	response := DomainGetPriceResponse{}
+	if err := json.Unmarshal(body, &response); err != nil {
+		return nil, errors.Wrap(err, "response unmarshal")
+	}
+
+	return &response, nil
+}
+
+type DomainGetPriceResponse struct {
+	Answer struct {
+		Currency   string                                  `json:"currency"`
+		PriceGroup string                                  `json:"price_group"`
+		Prices     map[string]DomainGetPriceResponseEntity `json:"prices"`
+	} `json:"answer"`
+	Charset      string      `json:"charset"`
+	Messagestore interface{} `json:"messagestore"`
+	Result       string      `json:"result"`
+}
+
+type DomainGetPriceResponseEntity struct {
+	ExtcreatePriceEqRenew int     `json:"extcreate_price_eq_renew"`
+	Idn                   int     `json:"idn"`
+	RegMaxPeriod          int     `json:"reg_max_period"`
+	RegMinPeriod          int     `json:"reg_min_period"`
+	RegPrice              float64 `json:"reg_price,string"`
+	RenewMaxPeriod        int     `json:"renew_max_period"`
+	RenewMinPeriod        int     `json:"renew_min_period"`
+	RenewPrice            float64 `json:"renew_price,string"`
+	RetailRegPrice        float64 `json:"retail_reg_price,string"`
+	RetailRenewPrice      float64 `json:"retail_renew_price,string"`
+}
